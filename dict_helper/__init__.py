cea252ce4da7e73777e3914772bef7cc0574c255
pegmatite <fmorisato@gmail.com>
2021-02-03 08:28:36 +0900

    Prepare package

---
+++ dict_helper/__init__.py
@@ -0,0 +1,11 @@
+"""
+Accessor for dictionary
+-----------------------
+  dig
+  dig_all
+  get
+"""
+
+from .dig import dig, dig_all, get
+from .over_iterator import over_iterator
+from .mix import mix
